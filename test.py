ed899083e31c58f4ff7b2cf819cf28e7f9cbb73a
LZING <505532111@qq.com>
2024-07-19 15:06:28 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-#test.pytest.pytest.py
+# config.py
+openai_api_key = "sk-2"
+openai_base_url = "https://35.333.top/v1"
+model_name = "gpt-3.5-turbo"
+
+# config.py
+
+class Config:
+    # Other configuration settings...
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:123123'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
